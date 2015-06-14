35b958ba7abf35e03099df6f96b086db7e14c496
Nicholas Guarino <ndguarino@cadmia.io>
2015-06-14 00:00:22 -0400

    Doesn't reinstall ZSH if exists now

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo "Installing ZSH and oh-my-zsh"
 echo "Please enter your password when prompted"
-sudo apt-get install zsh
+if which zsh >/dev/null; then
+  echo "ZSH installed. Not apt-getting"
+else
+  sudo apt-get install zsh
+end
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 chsh -s `which zsh`
 
