e4ff8f375062d0eb759433188b6fe7b9a88d4c2a
Nicholas Guarino <falanyx@ns390431.ip-188-165-245.eu>
2015-06-14 07:40:29 +0200

    Removed broken submodules, added git clone calls to install script

--- install.sh
+++ install.sh
@@ -27,5 +27,17 @@ cp "$DIR/erathis.zsh-theme" ~/.oh-my-zsh/custom/themes/
 echo "Installing VIm files"
 cp "$DIR/.vimrc" ~/.vimrc
 cp -r "$DIR/.vim" ~/.vim
+echo "Grabbing vim modules"
+pushd
+cd ~/.vim/bundle
+git clone https://github.com/mattn/emmet-vim.git
+git clone https://github.com/othree/eregex.vim.git
+git clone https://github.com/vim-scripts/mru.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/vim-scripts/taglist.vim.git
+git clone https://github.com/bling/vim-airline.git
+git clone https://github.com/altercation/vim-colors-solarized
+git clone https://github.com/tpope/vim-fugitive
 
 echo "Installed. Please restart your session!"
