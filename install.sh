f57c5e11c87caca71b46959677cd630ed14ae15a
Nicholas Guarino <ndguarino@cadmia.io>
2016-02-02 03:56:40 +0000

    Added vim-airline-themes to bundle
    Added vim-airline-themes to bundle as to prevent error

--- install.sh
+++ install.sh
@@ -35,6 +35,7 @@ git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/vim-scripts/taglist.vim.git
 git clone https://github.com/bling/vim-airline.git
+git clone https://github.com/vim-airline/vim-airline-themes
 git clone https://github.com/altercation/vim-colors-solarized
 git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/mxw/vim-jsx.git
